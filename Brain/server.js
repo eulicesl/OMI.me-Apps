8b3a4dca97c74c45601b7eada1e24ea0aa608cde
Eulices <105620565+eulicesl@users.noreply.github.com>
2025-08-20 20:07:22 -0400

    Update server.js

--- Brain/server.js
+++ Brain/server.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2025 Neo (github.com/neooriginal)
- * All rights reserved.
- */
-
 require('dotenv').config();
 const express = require('express');
 const OpenAI = require('openai');
